18d5b079debb6f3e58d6e938fb0c1c5aec09283a
Layek Hakim <Layekh9@gmail.com>
2021-07-18 15:54:51 +0000

    fixing test script 1

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -10,28 +10,22 @@ source venv/bin/activate
 pip3 install -r test_requirements.txt
 
 
-# pytest coverage server
-cd server
-python3 -m pytest --cov=app
-cd ..
-
 # pytest coverage server_1
 cd service_1
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml 
+python3 -m pytest --cov=application
 cd ..
 
 # pytest coverage server_2
 cd service_2
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application 
 cd ..
 
 # pytest coverage server_3
 cd service_3
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application 
 cd ..
 
 # pytest coverage server_4
 cd service_4
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate 
+python3 -m pytest --cov=application 
 cd ..
