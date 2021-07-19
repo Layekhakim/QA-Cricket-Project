90e0ffc02efc11e91795c6141ec18b90b822580c
Layek Hakim <Layekh9@gmail.com>
2021-07-19 04:18:54 +0000

    adding deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#copy over compose yaml to worker node
-ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
+#copy over compose yaml to manager node
+ssh -i ~/.ssh/ansible_id_rsa jenkins@manager:/home/jenkins/docker-compose.yaml << EOF
     export DATABASE_URI=${DATABASE_URI}
     "docker stack deploy --compose-file docker-compose.yaml cricket_project"
 EOF 
