0acbdedddd9a40e7386d5ba7530184f954a0fe8c
Layek Hakim <Layekh9@gmail.com>
2021-07-19 05:38:52 +0000

    changes

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #copy over compose yaml to manager node
-ssh -i ~/.ssh/id_rsa qa@manager:/home/qa/docker-compose.yaml << EOF
+ssh -i ~/.ssh/id_rsa qa@manager:~/docker-compose.yaml << EOF
     export DATABASE_URI=${DATABASE_URI}
     "docker stack deploy --compose-file docker-compose.yaml cricket_project"
 EOF 
     docker stack deploy --compose-file docker-compose.yaml cricket_project
+EOF
