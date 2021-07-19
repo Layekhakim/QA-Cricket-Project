35e5353d72abc4e38a4a282df832d64002186049
Layek Hakim <Layekh9@gmail.com>
2021-07-19 05:51:25 +0000

    changes to deploy.sh file

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #copy over compose yaml to manager node
-ssh -i ~/.ssh/ansible_id_rsa jenkins@manager:~/docker-compose.yaml << EOF
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
+# docker stack deploy
+ssh -i ~/.ssh/ansible_id_rsa jenkins@manager:docker-compose.yaml << EOF
     export DATABASE_URI=${DATABASE_URI}
     "docker stack deploy --compose-file docker-compose.yaml cricket_project"
 EOF 
